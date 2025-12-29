ec4b6f226377de3a7d9a2188436ab22d1191f2ec
ShivamAgrahari055 <agrahari8840@gmail.com>
2025-12-29 22:48:03 +0530

    moved public files to root for github

--- logout.js
+++ logout.js
@@ -1,4 +1,4 @@
-import { auth } from "../firebase.js";
+import { auth } from "./firebase.js";
 import { signOut } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
 const btn = document.getElementById("logoutBtn");
--- map.js
+++ map.js
@@ -1,4 +1,4 @@
-import { auth, db } from "../firebase.js";
+import { auth, db } from "./firebase.js";
 import { collection, onSnapshot } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
 const map = L.map("map").setView([20.5937, 78.9629], 5);
